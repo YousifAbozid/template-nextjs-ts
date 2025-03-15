64eb21fd4c8a4ad42671788226c416ffccbabaf4
Yousif Abozid <yousif.abozid@yahoo.com>
2025-03-15 17:21:28 +0200

    add Prettier configuration and update code formatting

--- app/components/ThemeToggle.tsx
+++ app/components/ThemeToggle.tsx
@@ -1,32 +1,33 @@
-'use client'
+'use client';
 
-import { useEffect } from 'react'
-import useLocalStorage from 'use-local-storage'
+import { useEffect } from 'react';
+import useLocalStorage from 'use-local-storage';
 
 const ThemeToggle = () => {
-	const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-	const [darkMode, setDarkMode] = useLocalStorage(
-		'theme',
-		defaultDark ? 'dark' : 'light'
-	)
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [darkMode, setDarkMode] = useLocalStorage(
+    'theme',
+    defaultDark ? 'dark' : 'light'
+  );
 
-	useEffect(() => {
-		if (darkMode === 'dark') {
-			document.documentElement.classList.add('dark')
-		} else {
-			document.documentElement.classList.remove('dark')
-		}
-	}, [darkMode])
+  useEffect(() => {
+    if (darkMode === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
 
-	return (
-		<button
-			onClick={() => {
-				setDarkMode(darkMode === 'dark' ? 'light' : 'dark')
-			}}
-			className='p-2 rounded-md bg-gray-200 dark:bg-gray-800 dark:text-white'>
-			{darkMode === 'dark' ? '🌙 Dark Mode' : '☀️ Light Mode'}
-		</button>
-	)
-}
+  return (
+    <button
+      onClick={() => {
+        setDarkMode(darkMode === 'dark' ? 'light' : 'dark');
+      }}
+      className="p-2 rounded-md bg-gray-200 dark:bg-gray-800 dark:text-white"
+    >
+      {darkMode === 'dark' ? '🌙 Dark Mode' : '☀️ Light Mode'}
+    </button>
+  );
+};
 
-export default ThemeToggle
+export default ThemeToggle;
