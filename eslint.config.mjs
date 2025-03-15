1a4f437cba9a3d13dc3a799eaa8dc75f4f41a243
Yousif Abozid <yousif.abozid@yahoo.com>
2025-03-15 17:41:26 +0200

    add Next.js ESLint plugin and update configuration for improved linting support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import nextPlugin from '@next/eslint-plugin-next';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -11,13 +12,19 @@ export default [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
-    // Add this rule configuration to disable requiring React in scope
+    plugins: {
+      '@next/next': nextPlugin,
+    },
     rules: {
       'react/react-in-jsx-scope': 'off',
       'react/jsx-uses-react': 'off',
       // Add these rules to catch console.log and no-unused-vars
       'no-console': ['error', { allow: ['warn', 'error'] }],
       '@typescript-eslint/no-unused-vars': 'error',
+
+      // Include recommended Next.js rules
+      '@next/next/no-html-link-for-pages': 'error',
+      '@next/next/no-img-element': 'error',
     },
     settings: {
       react: {
